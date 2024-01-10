72252b80235d7d31c3eafec3bc0f5228c1d23d79
qw1et <qwbykov30@mail.ru>
2024-01-10 03:14:03 +0300

    tabletk

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,14 @@ import requestDialogInit from "./modules/dialog-window"
 import requestDialogReg from "./modules/dialog-reg"
 import authInit from "./modules/btn-hide"
 import menuInit from "./modules/toggle-menu"
+import swiper from "./modules/slider"
 
 const init = () => {
     requestDialogInit()
     requestDialogReg()
     authInit()
     menuInit()
-    
+    swiper()
 }
- init ()
+
+init ()
